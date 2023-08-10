11ef8d3ecc89a5d9692a92feebdd2e0422edc89d
mayankpandey1099 <80150662+mayankpandey1099@users.noreply.github.com>
2023-08-10 22:06:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,10 +32,15 @@ document.addEventListener("DOMContentLoaded", () => {
             <p>Age: ${bookingdata.age}</p>
             <p>Name: ${bookingdata.name}</p>
             <p>Phone: ${bookingdata.phone}</p>
+            <button class="delete-button" data-id="${bookingdata._id}">Delete</button>
         `;
     bookingList.appendChild(item);
-  }
 
+    const deleteButton = item.querySelector(".delete-button");
+    deleteButton.addEventListener("click", () => {
+      deleteBooking(bookingdata._id, item); // Pass the item to be removed
+    });
+  }
   function addBookingToServer(bookingdata) {
     // Replace with your actual API endpoint
     const apiUrl =
@@ -51,15 +56,32 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   }
 
+  function deleteBooking(bookingId, element) {
+    // Pass the element to be removed
+    const apiUrl = `https://crudcrud.com/api/354757e353ce4a75bca0b8791aeba740/bookingList/${bookingId}`;
+
+    axios
+      .delete(apiUrl)
+      .then(() => {
+        console.log("Booking deleted successfully");
+        // Remove the deleted user detail from the display
+        if (element) {
+          bookingList.removeChild(element);
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting booking:", error);
+      });
+  }
+
   bookingForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const age = ageInput.value;
     const name = nameInput.value;
     const phone = phoneInput.value;
     const bookingdata = { age, name, phone };
-
-    if (age === '' || name === '' || phone === '') {
-      alert('Please fill in all required fields.');
+    if (age === "" || name === "" || phone === "") {
+      alert("Please fill in all required fields.");
       return; // Stop form submission
     }
 
