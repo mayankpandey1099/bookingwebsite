4a5ead5c11a4fff0715bcf6cd7d77ea2c055ee99
mayankpandey1099 <80150662+mayankpandey1099@users.noreply.github.com>
2023-08-10 18:31:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const bookingForm = document.getElementById("bookForm");
+  const ageInput = document.getElementById("age");
+  const nameInput = document.getElementById("name");
+  const phoneInput = document.getElementById("phone");
+  const bookingList = document.getElementById("bookingList");
+
+  function renderbookings() {
+    // Clear the booking list before rendering
+    bookingList.innerHTML = "";
+
+    // Replace with your actual API endpoint
+    const apiUrl =
+      "https://crudcrud.com/api/354757e353ce4a75bca0b8791aeba740/bookingList";
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        const bookings = response.data;
+        bookings.forEach((booking) => {
+          renderbooking(booking);
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching bookings:", error);
+      });
+  }
+
+  function renderbooking(bookingdata) {
+    const item = document.createElement("div");
+    item.innerHTML = `
+            <p>Age: ${bookingdata.age}</p>
+            <p>Name: ${bookingdata.name}</p>
+            <p>Phone: ${bookingdata.phone}</p>
+        `;
+    bookingList.appendChild(item);
+  }
+
+  function addBookingToServer(bookingdata) {
+    // Replace with your actual API endpoint
+    const apiUrl =
+      "https://crudcrud.com/api/354757e353ce4a75bca0b8791aeba740/bookingList";
+
+    axios
+      .post(apiUrl, bookingdata)
+      .then((response) => {
+        renderbooking(response.data); // Display the added booking on the screen
+      })
+      .catch((error) => {
+        console.error("Error adding bookings:", error);
+      });
+  }
+
+  bookingForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const age = ageInput.value;
+    const name = nameInput.value;
+    const phone = phoneInput.value;
+    const bookingdata = { age, name, phone };
+
+    addBookingToServer(bookingdata); // Add booking to the server
+    ageInput.value = "";
+    nameInput.value = "";
+    phoneInput.value = ""; // Clear phone input as well
+  });
+
+  renderbookings(); // Render initial bookings when the page loads
+});
 
